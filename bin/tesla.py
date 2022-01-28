5e1860f90f9adb55e0e49dcec92775aa55ef0790
erik1908 <erikgrasman@icloud.com>
2022-01-28 10:09:33 +0100

    dunno

--- bin/tesla.py
+++ bin/tesla.py
@@ -1,9 +1,31 @@
 import sys
 sys.path.append('/usr/local/lib/python3.8/site-packages')
 
-import teslajson
 import json
+import teslapy
+from teslapy import Tesla, Vehicle, Battery, SolarPanel
 
+import webview
+
+def custom_auth(url):
+    result = ['']
+    window = webview.create_window('Login', url)
+    def on_loaded():
+        result[0] = window.get_current_url()
+        if 'void/callback' in result[0].split('?')[0]:
+            window.destroy()
+    window.loaded += on_loaded
+    webview.start()
+    return result[0]
+
+with teslapy.Tesla('erik.grasman@umbrio.com', authenticator=custom_auth) as tesla:
+    tesla.fetch_token()
+    vehicles = tesla.vehicle_list()
+    print(vehicles)
+#    print(vehicles[0].get_vehicle_data()['vehicle_state']['car_version'])
+
+    
+'''
 c = teslajson.Connection('YOUREMAIL', 'YOURPASSWORD')
 v = c.vehicles[0]
 v.wake_up()
@@ -21,3 +43,4 @@ m = json.dumps(m)
 q = json.dumps(q)
 
 print("{ \"charger_state\":" + i + ",\"drive_state\":" + j + ",\"climate_state\":" + k + ",\"gui_settings\":" + l + ",\"vehicle_state\":" + m + ",\"vehicle_config\":" + q + "}")
+'''
